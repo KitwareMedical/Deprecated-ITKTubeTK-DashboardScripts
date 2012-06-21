430a3912d050a843cc624483ecfcda2cb15b35fb
Stephen R. Aylward <stephen.aylward@kitware.com>
2012-06-21 15:22:03 -0400

    ENH: Updated master script to self-update

--- Eternia_Kitware_TubeTK_Nightly.sh
+++ Eternia_Kitware_TubeTK_Nightly.sh
@@ -1,3 +1,21 @@
 #! /bin/sh
-cd /home/aylward/src/dashboards
-/usr/local/bin/ctest -S TubeTK_Dashboards/Eternia_Kitware_TubeTK_Nightly.cmake -D Nightly -V -VV -O Eternia_Kitware_TubeTK_Nightly.log
+
+MachineName=Eternia_Kitware
+
+if [ $1 != NoUpdate ]; then
+  echo "Updating"
+  # Update Dashboard repository
+  cd /home/aylward/src/dashboards/TubeTK_Dashboards
+  git pull -f
+
+  # Get new nightly script
+  cp -f ${MachineName}_TubeTK_Nightly.sh ..
+  cd ..
+  chmod +x ${MachineName}_TubeTK_Nightly.sh
+  
+  # Run new nightly script without updating again
+  ./${MachineName}_TubeTK_Nightly.sh NoUpdate
+fi
+
+# Run the nightly
+/usr/local/bin/ctest -S TubeTK_Dashboards/${MachineName}_TubeTK_Nightly.cmake -D Nightly -V -VV -O ${machineName}_TubeTK_Nightly.log
