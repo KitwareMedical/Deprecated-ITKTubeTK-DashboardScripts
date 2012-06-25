2a5b3202bfb084ba20a2f035ae50ba075b7accdd
Stephen R. Aylward <stephen.aylward@kitware.com>
2012-06-25 12:58:21 -0400

    ENH: Adding Ginger - draft

---
+++ Ginger_Aylward_Org_TubeTK_Nightly.bat
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+MachineName=Dash21_Kitware
+
+if [ $# -eq 0 ] || [ "$1" != "NoUpdate" ]; then
+  echo "Updating"
+  rm -rf /home/kitware/Dashboards/TubeTK-Debug
+
+  # Update Dashboard repository
+  cd /home/kitware/Dashboards/TubeTK_Dashboards
+  git pull -f
+
+  # Get new nightly script
+  cp -f ${MachineName}_TubeTK_Nightly.sh ..
+  cd ..
+  chmod +x ${MachineName}_TubeTK_Nightly.sh
+  
+  # Run new nightly script without updating again
+  ./${MachineName}_TubeTK_Nightly.sh NoUpdate
+fi
+
+# Run the nightly
+/home/kitware/Dashboards/Support/cmake-2.8.8/bin/ctest -S TubeTK_Dashboards/${MachineName}_TubeTK_Nightly.cmake -D Nightly -V -VV -O ${MachineName}_TubeTK_Nightly.log
