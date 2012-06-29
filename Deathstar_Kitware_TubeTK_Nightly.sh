d85a74bc048d4302919d038e891bfce8c5ec4cea
Matt McCormick <matt.mccormick@kitware.com>
2012-06-28 21:57:13 -0400

    ENH: Add scripts for deathstar.

---
+++ Deathstar_Kitware_TubeTK_Nightly.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+MachineName=Deathstar_Kitware
+
+echo "Running TubeTK Dashboard script"
+if [ $# -eq 0 ] || [ "$1" != "NoUpdate" ]; then
+  echo "Updating"
+  rm -rf /home/matt/dashboards/TubeTK-RelWithDebInfo
+ 
+  # Update Dashboard repository
+  cd /home/matt/dashboards/TubeTK_Dashboards
+  git pull -f
+ 
+  echo "Bootstrapping"
+  # Get new nightly script
+  cp -f ${MachineName}_TubeTK_Nightly.sh ..
+  cd ..
+  chmod +x ${MachineName}_TubeTK_Nightly.sh
+  
+  # Run new nightly script without updating again
+  ./${MachineName}_TubeTK_Nightly.sh NoUpdate
+else
+# Run the nightly
+  /usr/local/bin/ctest -S TubeTK_Dashboards/${MachineName}_TubeTK_Nightly.cmake -V -VV -O ${MachineName}_TubeTK_Nightly.log
+fi
+echo "Ending TubeTK Dashboard script"
