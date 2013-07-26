fdec967c25e729e83f4fceb2103875aef81367ca
Jamie Snape <jamie.snape@kitware.com>
2013-07-26 13:02:38 -0400

    STYLE: Remove spaces at end of lines

--- Deathstar_Kitware_TubeTK_Nightly.sh
+++ Deathstar_Kitware_TubeTK_Nightly.sh
@@ -7,17 +7,17 @@ if [ $# -eq 0 ] || [ "$1" != "NoUpdate" ]; then
 
   echo "Updating"
   rm -rf /home/matt/dashboards/TubeTK-RelWithDebInfo
- 
+
   # Update Dashboard repository
   cd /home/matt/dashboards/TubeTK_Dashboards
   git pull
- 
+
   echo "Bootstrapping"
   # Get new nightly script
   cp -f ${MachineName}_TubeTK_Nightly.sh ..
   cd ..
   chmod +x ${MachineName}_TubeTK_Nightly.sh
-  
+
   # Run new nightly script without updating again
   ./${MachineName}_TubeTK_Nightly.sh NoUpdate
 
