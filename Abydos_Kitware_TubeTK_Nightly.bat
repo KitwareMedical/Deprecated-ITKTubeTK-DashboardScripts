8c6032bb133ecbf3b62bf9919439a892948a62be
Stephen R. Aylward <stephen.aylward@kitware.com>
2013-12-30 16:55:01 -0500

    ENH: Abydos set for continuous build

--- Abydos_Kitware_TubeTK_Nightly.bat
+++ Abydos_Kitware_TubeTK_Nightly.bat
@@ -8,4 +8,7 @@ cd C:\dashboard\src\TubeTK-DashboardScripts
 cd ..
 
 REM Run the nightly CTest
-"C:\Program Files\CMake 2.8\bin\ctest.exe" -S "C:\dashboard\src\TubeTK-DashboardScripts\Abydos_Kitware_TubeTK.cmake" -C Release -O "C:\dashboard\src\Abydos_Kitware_TubeTK_Nightly.log"
+"C:\Program Files\CMake 2.8\bin\ctest.exe" -D Nightly -D SITE_CTEST_MODE:STRING=Nightly -D SITE_BUILD_TYPE:STRING=Release -S "C:\dashboard\src\TubeTK-DashboardScripts\Abydos_Kitware_TubeTK.cmake" -C Release -V -VV -O "C:\dashboard\src\Abydos_Kitware_TubeTK_Nightly.log"
+
+REM Run the continuous CTest
+"C:\Program Files\CMake 2.8\bin\ctest.exe" -D Continuous -D SITE_CTEST_MODE:STRING=Continuous -D SITE_BUILD_TYPE:STRING=Release -S "C:\dashboard\src\TubeTK-DashboardScripts\Abydos_Kitware_TubeTK.cmake" -C Release -V -VV -O "C:\dashboard\src\Abydos_Kitware_TubeTK_Continuous.log"
