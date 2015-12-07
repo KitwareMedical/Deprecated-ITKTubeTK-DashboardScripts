c09c6fb1fbeca1db6dd49a8d934bcbb46545fe5c
Stephen R. Aylward <stephen.aylward@kitware.com>
2015-12-06 19:53:43 -0500

    ENH: Escape quotes for generator

--- run_nightly.cmake
+++ run_nightly.cmake
@@ -61,8 +61,9 @@ if( SITE_NIGHTLY_BUILD )
   ctest_update( SOURCE "${TubeTK_SOURCE_DIR}" )
   ctest_configure( BUILD "${TubeTK_BINARY_DIR}"
     SOURCE "${TubeTK_SOURCE_DIR}"
-    OPTIONS "-C${TubeTK_BINARY_DIR}/InitCMakeCache.cmake" 
-      "-G${CMAKE_GENERATOR}" )
+    OPTIONS
+      " -C${TubeTK_BINARY_DIR}/InitCMakeCache.cmake
+      -G\"${CMAKE_GENERATOR}\" " )
   ctest_read_custom_files( "${TubeTK_BINARY_DIR}" )
   ctest_build( BUILD "${TubeTK_BINARY_DIR}" )
   ctest_submit( PARTS Notes Update Configure Build )
