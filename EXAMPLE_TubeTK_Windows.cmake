b0c3331edb20de7aef378377742ee5a542163379
Francois Budin <francois.budin@gmail.com>
2016-07-15 10:29:47 -0400

    ENH: Skipping TubeTK_TEST_ENVIRONMENT test
    TubeTK_TEST_ENVIRONMENT verifies that the current environment settings matches
    the current project setting. ITK_BUILD_DIR and TubeTK_BUILD_DIR must be set
    as environment variables and match the current project settings. On
    dashboard machines, multiple build typically happen on one machine. Therefore
    environment variables cannot possibly match the setting for all build.
    This test is not relevant on dashboard machine and therefore is disabled.

--- EXAMPLE_TubeTK_Windows.cmake
+++ EXAMPLE_TubeTK_Windows.cmake
@@ -130,6 +130,7 @@ set( TubeTK_USE_PYTHON ON )
 set( TubeTK_USE_QT ON )
 set( TubeTK_USE_VALGRIND OFF )
 set( TubeTK_USE_VTK ON )
+set( TubeTK_TEST_ENVIRONMENT OFF)
 
 #
 # Configure what is run on this machine for experimental, continuous, and 
