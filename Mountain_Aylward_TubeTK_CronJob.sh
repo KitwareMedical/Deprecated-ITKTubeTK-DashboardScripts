26832b2d2db67b57649a1944ad9b0a6deea7baa7
Stephen R. Aylward <stephen.aylward@kitware.com>
2017-08-06 20:50:35 -0400

    Enh: Adding single cron file for Mountain

---
+++ Mountain_Aylward_TubeTK_CronJob.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/home/aylward/src/ITKTubeTK-DashboardScripts/Mountain_Aylward_TubeTK_Nightly-Debug.sh
+/home/aylward/src/ITKTubeTK-DashboardScripts/Mountain_Aylward_TubeTK_Nightly-Release.sh
