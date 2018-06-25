f4ff09b60d8d2f4602c942c18c1bb59f7de58f1e
Hastings Greer <hastings.greer@kitware.com>
2018-06-25 13:26:03 +0000

    New Ubuntu Dashboard Scripts

---
+++ NIH_Amazon_TubeTK_CronJob.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+/home/ubuntu/src/ITKTubeTK-DashboardScripts/Mountain_Aylward_TubeTK_Nightly-Debug.sh
+/home/ubuntu/src/ITKTubeTK-DashboardScripts/Mountain_Aylward_TubeTK_Nightly-Release.sh
