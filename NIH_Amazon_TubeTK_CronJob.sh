eee437197fc886421f7f349ebf5f6d3a4da74f11
Hastings Greer <hastings.greer@kitware.com>
2018-10-17 23:21:10 +0000

    Final fix of cronjob. Had fields confused

--- NIH_Amazon_TubeTK_CronJob.sh
+++ NIH_Amazon_TubeTK_CronJob.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-/home/ubuntu/src/ITKTubeTK-DashboardScripts/NIH_Amazon_TubeTK_Nightly-Debug.sh
 /home/ubuntu/src/ITKTubeTK-DashboardScripts/NIH_Amazon_TubeTK_Nightly-Release.sh
+/home/ubuntu/src/ITKTubeTK-DashboardScripts/NIH_Amazon_TubeTK_Nightly-Debug.sh
